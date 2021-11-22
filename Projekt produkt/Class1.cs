f66e49cd7c19c9324361099cb698ad72d76a3a46
Szkola <Szkola@S24K01>
2021-11-22 12:15:37 +0100

    Dodaj pliki projektów.

---
+++ Projekt produkt/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt_produkt
+{
+    class Class1
+    {
+    }
+}
---
+++ Projekt produkt/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt_produkt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Produkt p1 = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWiazanie();
+        }
+        private void PrzygotujWiazanie()
+        {
+            p1 = new Produkt("Dz-10", "długopis żelowy", 132, "Katowice 1");
+            gridProdukt.DataContext = p1;
+        }
+
+        private void btnPotwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            string tekst = String.Format("{0}{1}{2}", "Wprowadzona dane:",
+                Environment.NewLine, p1.ToString());
+            MessageBox.Show(tekst);
+        }
+    }
+}
